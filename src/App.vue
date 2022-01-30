6891bf5116a4dfb673dff60c7f2d2062218bbf33
Marcio Alves Martins <41830609+MarcioAMartins@users.noreply.github.com>
2022-01-30 10:57:26 -0300

    Create Header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
-    
+    <Header />
   </div>
 </template>
 
 <script>
 import './styles/global.css'
+import Header from './components/Header';
 
 export default {
   name: 'App',
   components: {
+    Header
       }
 }
 </script>
